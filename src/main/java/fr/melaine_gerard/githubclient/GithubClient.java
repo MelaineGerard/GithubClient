f2f203b40c369cdd0812fea740be02a1fdad9207
Melaine Gérard <pro@melaine-gerard.fr>
2021-02-16 22:36:37 +0100

    Initial Commit

---
+++ src/main/java/fr/melaine_gerard/githubclient/GithubClient.java
@@ -0,0 +1,8 @@
+package fr.melaine_gerard.githubclient;
+
+public class GithubClient {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Github Client !");
+    }
+}
